91daff8d9815cb93d791d5ae1e6d1de43b5bca50
Michael Levine <melevine45@gmail.com>
2021-12-28 00:18:52 -0500

    Added use-package and a list of required packages to install at startup if they are not already installed

--- init.el
+++ init.el
@@ -5,6 +5,24 @@
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(package-install-selected-packages)
+
+;; Make sure your required packages are installed on startup.
+(defvar packages-required-at-startup
+  '(haskell-mode lsp lsp-haskell go-mode lsp-mode use-package))
+
+(defun packages-required-at-startup-are-installed-p ()
+  (loop for p in package-required-at-startup
+	when (not (package-installed-p p)) do (return nil)
+	finally (return t)))
+
+(unless (packages-required-at-startup-are-installed-p)
+  (package-refresh-contents)
+  (dolist (p packages-required-at-startup)
+    (when (not (package-installed-p p))
+      (package-install p))))
 
 ;; revert buffers automatically when underlying files are changed externally
 (global-auto-revert-mode t)
@@ -21,6 +39,10 @@
 ;; File matching with major modes
 ;;(add-to-list 'auto-mode-alist '("\\.hs\\'" . haskell-mode
 
+;;use-package settings
+(eval-when-compile
+  (require 'use-package))
+
 ;; Haskell mode
 (require 'haskell-mode)			 
 (require 'lsp)
@@ -30,3 +52,17 @@
 (add-hook 'haskell-literate-mode-hook #'lsp)
 
 ;; Go mode
+(require 'go-mode)
+(require 'lsp-mode)
+(add-hook 'go-mode-hook #'lsp-deferred)
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+;; All the available customizations: https://github.com/emacs-lsp/lsp-mode/blob/master/clients/lsp-go.el 
+(setq lsp-go-hover-kind "FullDocumentation")
+(setq lsp-go-links-in-hover t)
+;; (setq lsp-go-use-gofumpt nil)
+;;(setq lsp-go-import-shortcut "Both")
